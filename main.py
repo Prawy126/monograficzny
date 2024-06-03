8d2b68203d81ca109e630697ed9287425d75d6b5
MateuszSondej21 <mateusz.sondej2@interia.pl>
2024-06-03 21:21:28 +0200

    Poprawiony kod o komentarze wyjaśniające działanie instrukcji, spolszczenie zmiennych, funkcji i komentarzy oraz zaktualizowanie index.md

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 # Definicja przedziałowych zbiorów rozmytych
 A = [[0.0, 1.0], [0.5, 1.0], [0.6, 0.8], [0.4, 0.7], [0.0, 1.0], [0.0, 1.0]]
 B = [[0.4, 0.7], [0.0, 1.0], [0.6, 0.9], [0.4, 0.8], [1.0, 1.0], [1.0, 1.0]]
@@ -7,113 +5,108 @@ C = [[1.0, 1.0], [0.0, 1.0], [0.9, 0.9], [0.0, 1.0], [1.0, 1.0], [1.0, 1.0]]
 D = [[0.4, 0.7], [0.2, 1.0], [0.2, 1.0], [0.4, 0.8], [0.5, 1.0], [0.5, 0.8]]
 E = [[0.5, 0.6], [0.5, 1.0], [0.6, 0.8], [0.5, 0.9], [0.8, 1.0], [0.8, 1.0]]
 
-
 # Negacja przedziału
-def negation(interval):
-    return [round(1 - interval[1], 4), round(1 - interval[0], 4)]
-
+def negacja(przedzial):
+    return [round(1 - przedzial[1], 4), round(1 - przedzial[0], 4)]
 
 # Agregacja
-def aggregation(x, y):
+def agregacja(x, y):
     return [round((x[0] + y[0]) / 2, 4), round((x[1] + y[1]) / 2, 4)]
 
-
-def part_order(x, y):
+def porzadek_czesciowy(x, y):
     if x[0] <= y[0] and x[1] <= y[1]:
         return True
     return False
 
-
 # Miara pierwszeństwa
-def precedence(x, y):
+def pierwszenstwo(x, y):
     if x == y:
         return [round(1 - (x[1] - x[0]), 4), 1]
-    elif part_order(x, y):
+    elif porzadek_czesciowy(x, y):
         return [1, 1]
     else:
         l = round((1 - x[1] + y[0]) / 2, 4)
         u = round((1 - x[0] + y[1]) / 2, 4)
         return [l, u]
 
-
 # Miara podobieństwa
-def similarity(A, AN):
-    s_values = []
+def podobienstwo(A, AN):
+    wartosci_podobienstwa = []
     for a, an in zip(A, AN):
-        prec_1 = precedence(a, an)
-        prec_2 = precedence(an, a)
-        s = min(prec_1[0], prec_2[0]), min(prec_1[1], prec_2[1])
-        s_values.append(s)
-    return s_values
+        pierwsz_1 = pierwszenstwo(a, an)
+        pierwsz_2 = pierwszenstwo(an, a)
+        s = min(pierwsz_1[0], pierwsz_2[0]), min(pierwsz_1[1], pierwsz_2[1])
+        wartosci_podobienstwa.append(s)
+    return wartosci_podobienstwa
 
-
-def mean_interval(interval1, interval2):
+# Średnia z dwóch przedziałów
+def srednia_przedzialow(przedzial1, przedzial2):
     """
-    Calculates the mean interval of two intervals.
+    Oblicza średnią dwóch przedziałów.
     """
-    return [round((interval1[0] + interval2[0]) / 2, 4), round((interval1[1] + interval2[1]) / 2, 4)]
+    return [round((przedzial1[0] + przedzial2[0]) / 2, 4), round((przedzial1[1] + przedzial2[1]) / 2, 4)]
 
-
-def process_intervals(intervals, name):
+# Przetwarzanie listy przedziałów
+def przetwarzanie_przedzialow(przedzialy, nazwa):
     """
-    Processes a list of intervals using the mean_interval function until only one interval remains.
-    Prints the set name and iteration number for each iteration.
+    Przetwarza listę przedziałów za pomocą funkcji srednia_przedzialow, aż pozostanie tylko jeden przedział.
+    Wyświetla nazwę zbioru i numer iteracji dla każdej iteracji.
     """
-    print(f"Zbiór {name}:")
-    print("   Iteracja 0:", ", ".join(map(str, intervals)))  # Wyświetlenie początkowego zestawu przedziałów
-    iteration = 1
-    while len(intervals) > 1:
-        result = []
-        length = len(intervals)
+    print(f"Zbiór {nazwa}:")
+    print("   Iteracja 0:", ", ".join(map(str, przedzialy)))  # Wyświetlenie początkowego zestawu przedziałów
+    iteracja = 1
+    while len(przedzialy) > 1:
+        wynik = []
+        dlugosc = len(przedzialy)
 
-        # Iterate over the intervals in pairs
-        for i in range(0, length - 1, 2):
-            result.append(mean_interval(intervals[i], intervals[i + 1]))
+        # Iteracja przez przedziały parami
+        for i in range(0, dlugosc - 1, 2):
+            wynik.append(srednia_przedzialow(przedzialy[i], przedzialy[i + 1]))
 
-        # If the number of intervals is odd, carry the last unpaired interval to the next round
-        if length % 2 != 0:
-            result.append(intervals[-1])
+        # Jeśli liczba przedziałów jest nieparzysta, przenieś ostatni nieparzysty przedział do następnej rundy
+        if dlugosc % 2 != 0:
+            wynik.append(przedzialy[-1])
 
-        intervals = result
+        przedzialy = wynik
 
-        # Print the set name and iteration number with indentation
-        indentation = " " * (6 * (iteration - 1)) + " " * (3 * (iteration - 1))
-        print(" " * 15, indentation, "Iteracja", iteration, ":", ", ".join(map(str, intervals)))
+        # Drukowanie nazwy zbioru i numeru iteracji z wcięciem
+        wciecie = " " * (6 * (iteracja - 1)) + " " * (3 * (iteracja - 1))
+        print(" " * 15, wciecie, "Iteracja", iteracja, ":", ", ".join(map(str, przedzialy)))
 
-        iteration += 1
+        iteracja += 1
 
-    return intervals[0]
+    return przedzialy[0]
 
-# Pozostała część kodu bez zmian
-AN = [negation(interval) for interval in A]
-BN = [negation(interval) for interval in B]
-CN = [negation(interval) for interval in C]
-DN = [negation(interval) for interval in D]
-EN = [negation(interval) for interval in E]
+# Obliczenie negacji dla każdego zbioru
+AN = [negacja(przedzial) for przedzial in A]
+BN = [negacja(przedzial) for przedzial in B]
+CN = [negacja(przedzial) for przedzial in C]
+DN = [negacja(przedzial) for przedzial in D]
+EN = [negacja(przedzial) for przedzial in E]
 
-results = []
+wyniki = []
 
+# Przetwarzanie i wyświetlanie wyników dla każdego zbioru
 print("Wyniki dla zbiorów:")
-result_A = process_intervals(similarity(A, AN), 'A')
-results.append(('A', result_A))
+wynik_A = przetwarzanie_przedzialow(podobienstwo(A, AN), 'A')
+wyniki.append(('A', wynik_A))
 
-result_B = process_intervals(similarity(B, BN), 'B')
-results.append(('B', result_B))
+wynik_B = przetwarzanie_przedzialow(podobienstwo(B, BN), 'B')
+wyniki.append(('B', wynik_B))
 
-result_C = process_intervals(similarity(C, CN), 'C')
-results.append(('C', result_C))
+wynik_C = przetwarzanie_przedzialow(podobienstwo(C, CN), 'C')
+wyniki.append(('C', wynik_C))
 
-result_D = process_intervals(similarity(D, DN), 'D')
-results.append(('D', result_D))
+wynik_D = przetwarzanie_przedzialow(podobienstwo(D, DN), 'D')
+wyniki.append(('D', wynik_D))
 
-result_E = process_intervals(similarity(E, EN), 'E')
-results.append(('E', result_E))
+wynik_E = przetwarzanie_przedzialow(podobienstwo(E, EN), 'E')
+wyniki.append(('E', wynik_E))
 
 # Sortowanie wyników według warunku x ≤ y
-sorted_results = sorted(results, key=lambda x: x[1][0])
+posortowane_wyniki = sorted(wyniki, key=lambda x: x[1][0])
 
-print("\nPosortowane wyniki:")
-for name, interval in sorted_results:
-    print(f"Zbiór {name}: {interval}")
+print("\nWyniki po sortowaniu possible:")
 
-
+for nazwa, przedzial in posortowane_wyniki:
+    print(f"Zbiór {nazwa}: {przedzial}")
